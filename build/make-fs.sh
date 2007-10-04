84de933d47ea27e58eb44172ef2278bc821d608d
Mitch Bradley <wmb@laptop.org>
2007-10-03 22:12:15 -1000

    Secure filesystem update support.

---
+++ build/make-fs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Make a filesystem update bundle
+# Usage: make-fs input-filename
+# Example:
+# make-fs  os612.img
+# The output file is fs.zip
+
+[ $# != 1 ] && echo Usage: $0 osname && exit 1
+
+infile=$1
+buildname=`basename $infile .img`
+hashname=sha256
+# hashname=rmd160
+outfile=fs.zip
+
+./hashfs $hashname $infile data.img
+echo $buildname >version.txt
+
+./sig01 sha256 fs data.img >data.sig
+rm -f $outfile
+zip -n .sig:.img:.txt $outfile data.sig version.txt data.img
+rm -f data.sig data.img version.txt

e2de4d4e6e22f97c6767570f4b4e18672d1a1def
Martin Langhoff <martin@laptop.org>
2009-05-08 12:21:00 +0200

    make-lease: now handles absolute expiry -- remove make-lease-abs

--- build/make-lease.sh
+++ build/make-lease.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Make a lease file
-# Usage: make-lease.sh serial-number uuid
+# Usage: make-lease.sh serial-number uuid [days|expire] [outfile]
 # Example:
 # make-lease.sh SHF706002A7 8BF9AC40-26F8-4BCC-A699-BE51FD366419 1 [outfile]
 
-[ $# -lt 3 ] && echo Usage: $0 serial-number uuid days [outfile] && exit 1
+[ $# -lt 3 ] && echo Usage: $0 serial-number uuid [days|expire] [outfile] && exit 1
 
 sn=$1
 uu=$2
-days=$3
+expire=$3
 
 if [ $# -ge 4 ]; then
   outfile=$4
@@ -17,5 +17,12 @@ else
   outfile=/dev/stdout
 fi
 
-expire=`./futureday.py $days`
+if [[ ${#expire} != 16 || "${expire:8:1}" != "T" || "${expire:15:1}" != "Z" ]]; then
+    expire=`./futureday.py $expire`
+    if [ $? != 0 ]; then
+	echo "unrecognised date format" >> /dev/stderr
+	exit 1
+    fi
+fi
+
 echo act01: $sn K $expire `echo -n $sn:$uu:K:$expire | ./sig01 sha256 lease` >$outfile

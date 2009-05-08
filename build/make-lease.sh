83c3ef64df186d3798975627283514ba62e7d32b
Martin Langhoff <martin@laptop.org>
2009-05-08 13:24:25 +0200

    make-lease: handle sig02 leases, simple and chained, allow naming the key

--- build/make-lease.sh
+++ build/make-lease.sh
@@ -1,11 +1,41 @@
 #!/bin/sh
 
 # Make a lease file
-# Usage: make-lease.sh serial-number uuid [days|expire] [outfile]
+# Usage: make-lease.sh [--v2] [--chain delegfile] [--signingkey keyname] serial-number uuid [days|expire] [outfile]
 # Example:
 # make-lease.sh SHF706002A7 8BF9AC40-26F8-4BCC-A699-BE51FD366419 1 [outfile]
 
-[ $# -lt 3 ] && echo Usage: $0 serial-number uuid [days|expire] [outfile] && exit 1
+[ $# -lt 3 ] && echo Usage: $0 [--v2] [--chain delegfile] [--signingkey keyname] serial-number uuid [days|expire] [outfile] && exit 1
+
+# Handle opts
+fullkey=""
+chainfile=""
+signingkey="lease"
+v2=""
+
+while [ $# != 0 ] && [ ${1:0:1} == '-' ]; do
+    case "$1" in
+	--v2)
+	    v2=" --v2 "
+	    ;;
+	--fullkey)
+	    fullkey=" --fullkey ";
+	    ;;
+	--chain)
+	    chainfile=$2
+	    fullkey=" --fullkey "
+	    shift;
+	    ;;
+	--signingkey)
+	    signingkey=$2
+	    shift;
+	    ;;
+	*)
+	    echo "Unknown param $1" >> /dev/stderr
+	    exit 1;
+    esac
+    shift
+done
 
 sn=$1
 uu=$2
@@ -25,4 +55,18 @@ if [[ ${#expire} != 16 || "${expire:8:1}" != "T" || "${expire:15:1}" != "Z" ]]; 
     fi
 fi
 
-echo act01: $sn K $expire `echo -n $sn:$uu:K:$expire | ./sig01 sha256 lease` >$outfile
+if [ "$chainfile" == "" ]; then
+    if [ "$v2" == "" ]; then
+	( echo -n "act01: $sn K $expire "; 
+	    echo -n $sn:$uu:K:$expire | ./sig01 $fullkey sha256 $signingkey ) >$outfile
+    else 
+	# non-chained v2
+	( echo -n "act01: $sn K $expire "; 
+	    echo -n $sn:$uu:K:$expire | ./sig01 --v2 $expire $fullkey sha256 $signingkey ) >$outfile
+    fi
+else
+    # v2, chained
+    ( echo -n "act01: $sn K $expire ";
+	head -c -1 "$chainfile" ;
+	echo -n $sn:$uu:K:$expire | ./sig01 --v2 $expire $fullkey sha256 $signingkey | tail -c +7 ) >$outfile
+fi

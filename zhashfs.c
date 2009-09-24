19d2f1f142039a9ee11507f6e5f77f18e1c5888d
Mitch Bradley <wmb@laptop.org>
2009-09-24 10:50:03 -1000

    Added zhashfs.c and corresponding makefile rules.

--- zhashfs.c
+++ zhashfs.c
@@ -15,23 +15,17 @@ void run_cmd(int res, int line, char *file, char *cmd)
    }
 }
 
-#define EBLOCK_SIZE 0x20000
-/*
- * For zlib compress, the destination buffer needs to be 1.001 x the
- * src buffer size plus 12 bytes
- */
-#define ZBUF_EXTRA ((EBLOCK_SIZE / 999) + 12)
-// #define ZBUFSIZE (EBLOCK_SIZE + ZBUF_EXTRA)
-#define ZBUFSIZE (2 * EBLOCK_SIZE)
-
 int main(int argc, char **argv)
 {
     char          *fname;
     char          *hashname;
-    unsigned char buf[EBLOCK_SIZE], md[MAXBLOCKSIZE], sig[512];
+    unsigned char *buf;  // EBLOCKSIZE
+    unsigned char md[MAXBLOCKSIZE], sig[512];
     unsigned long mdlen;
     FILE          *infile, *outfile;
-    long          eblocks, i;
+    long          eblocks = -1;
+    long          i;
+    long	  zblocksize, zbufsize;
     off_t         insize;
     int		  hashid, readlen;
     int		  j;
@@ -41,77 +35,101 @@ int main(int argc, char **argv)
     FILE          *zfile;
     uLongf        zlen;
     unsigned char *p;
-    unsigned char zbuf[ZBUFSIZE];
+    unsigned char *zbuf; // ZBUFSIZE
 
-    if (argc < 5) { 
-        fprintf(stderr, "%s: hashname signed_file_name spec_file_name zdata_file_name\n", argv[0]);
+    if (argc < 6) { 
+        fprintf(stderr, "%s: zblocksize hashname signed_file_name spec_file_name zdata_file_name [ #blocks ]\n", argv[0]);
         return EXIT_FAILURE;
     }
+
+    if (argc == 7)
+        eblocks = strtol(argv[6], 0, 0);
+
+    zblocksize = strtol(argv[1], 0, 0);
+
+    buf = malloc(zblocksize);
+    LTC_ARGCHK(buf != 0);
+
+    /*
+     * For zlib compress, the destination buffer needs to be 1.001 x the
+     * src buffer size plus 12 bytes
+     */
+    zbufsize = ((zblocksize * 102) / 100) + 12;
+    zbuf = malloc(zbufsize);
+    LTC_ARGCHK(zbuf != 0);
 
     LTC_ARGCHK(register_hash(&sha256_desc) != -1);
     LTC_ARGCHK(register_hash(&rmd160_desc) != -1);
     LTC_ARGCHK(register_hash(&md5_desc) != -1);
     ltc_mp = tfm_desc;
 
-    hashname = argv[1];
+    hashname = argv[2];
     hashid = find_hash(hashname);
     LTC_ARGCHK(hashid >= 0);
 
     /* open filesystem image file */
-    infile = fopen(argv[2], "rb");
+    infile = fopen(argv[3], "rb");
     LTC_ARGCHK(infile != NULL);
 
     /* open output file */
-    outfile = fopen(argv[3], "wb");
+    outfile = fopen(argv[4], "wb");
     LTC_ARGCHK(outfile != NULL);
 
     /* open zdata file */
-    zfile = fopen(argv[4], "wb");
+    zfile = fopen(argv[5], "wb");
     LTC_ARGCHK(outfile != NULL);
 
-    (void)fseek(infile, 0L, SEEK_END);
-    insize = ftello(infile);
-    (void)fseek(infile, 0L, SEEK_SET);
+    if (eblocks == -1) {
+        (void)fseek(infile, 0L, SEEK_END);
+        insize = ftello(infile);
+        (void)fseek(infile, 0L, SEEK_SET);
 
-    eblocks = (insize + EBLOCK_SIZE - 1) / EBLOCK_SIZE;
-//    LTC_ARGCHK((eblocks * EBLOCK_SIZE) == insize);
+        eblocks = (insize + zblocksize - 1) / zblocksize;
+//    LTC_ARGCHK((eblocks * zblocksize) == insize);
+    }
 
     /* Remove possible path prefix */
-    fname = strrchr(argv[2], '/');
+    fname = strrchr(argv[3], '/');
     if (fname == NULL)
-        fname = argv[2];
+        fname = argv[3];
     else
         ++fname;
 
     fprintf(outfile, "data: %s\n", fname);
-    fprintf(outfile, "zblocks: %x %x\n", EBLOCK_SIZE, eblocks);
-    fprintf(zfile,   "zblocks: %x %x\n", EBLOCK_SIZE, eblocks);
+    fprintf(outfile, "zblocks: %x %x\n", zblocksize, eblocks);
+    fprintf(zfile,   "zblocks: %x %x\n", zblocksize, eblocks);
+
+    fprintf(stdout, "Total blocks: %d\n", eblocks);
 
     /* make a hash of the file */
     for (i=0; i < eblocks; i++) {
-        readlen = fread(buf, 1, EBLOCK_SIZE, infile);
-        if (readlen != EBLOCK_SIZE && readlen && i == eblocks-1) {
-            for (p = &buf[readlen]; p < &buf[EBLOCK_SIZE]; p++) {
+        readlen = fread(buf, 1, zblocksize, infile);
+        if (readlen != zblocksize && readlen && i == eblocks-1) {
+            for (p = &buf[readlen]; p < &buf[zblocksize]; p++) {
                 *p = 0xff;
             }
-            readlen = EBLOCK_SIZE;
+            readlen = zblocksize;
         }            
-        LTC_ARGCHK(readlen == EBLOCK_SIZE);
+        LTC_ARGCHK(readlen == zblocksize);
 
+#ifdef notdef
         allf = 1;
-        for (p = (unsigned char *)buf; p < &buf[EBLOCK_SIZE]; p++) {
+        for (p = (unsigned char *)buf; p < &buf[zblocksize]; p++) {
             if (*p != 0xff) {
                 allf = 0;
                 break;
             }
         }
+#else
+        allf = 0;
+#endif
 
         if (!allf) {
             mdlen = sizeof(md);
-            DO(hash_memory(hashid, buf, EBLOCK_SIZE, md, &mdlen));
+            DO(hash_memory(hashid, buf, zblocksize, md, &mdlen));
 
-            zlen = ZBUFSIZE;
-            if ((zresult = compress(zbuf, &zlen, buf, EBLOCK_SIZE)) != Z_OK) {
+            zlen = zbufsize;
+            if ((zresult = compress(zbuf, &zlen, buf, zblocksize)) != Z_OK) {
                 fprintf(stderr, "Compress failure at block 0x%x - %d\n", i, zresult);
             }
 
@@ -127,7 +145,7 @@ int main(int argc, char **argv)
             fwrite(zbuf, sizeof(char), zlen, zfile);
             fprintf(zfile, "\n");
         }
-
+        fprintf(stdout, "\r%d", i);  fflush(stdout);
     }
     fprintf(outfile, "zblocks-end:\n");
     fprintf(zfile,   "zblocks-end:\n");

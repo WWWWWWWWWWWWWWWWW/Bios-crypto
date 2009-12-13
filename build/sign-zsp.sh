a3fd4d3542784ce0974779f928f61a1f580c7a5a
Daniel Drake <dsd@laptop.org>
2009-12-13 12:11:26 +0000

    add script for signing zsp files for FS imaging

---
+++ build/sign-zsp.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Sign a .zsp file into a fs.zip
+# Usage: sign-zsp.sh keyname infile
+# Example: sign-zsp.sh fs os99.zsp
+# Output file is fs.zip
+
+# Ensure we call the binaries that are in the same
+# directory as this shell script
+MYPATH=$(readlink -f $0)
+LIBEXEC=$(dirname $MYPATH)
+
+signingkey=$1
+infile=$2
+outfile=fs.zip
+
+echo "data: `cat $2`" > data.img
+$LIBEXEC/sig01 sha256 $signingkey data.img > data.sig
+rm -f $outfile
+zip $outfile data.sig data.img
+rm -f data.sig data.img
+

920c27ad95f169bf2280935eccc688aad0fe5796
Martin Langhoff <martin@laptop.org>
2010-04-15 16:46:52 -0300

    add handy shell utils for deployment teams

---
+++ build/cp-server-delegations.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Create a tree with the files server.pub & server.pri in a directory for server
+# Usage: cp-server-dirs src dest
+# Example:
+# 
+# Notes:
+#
+
+set -e
+
+# Ensure we call the binaries that are in the same
+# directory as this shell script
+MYPATH=$(readlink -f $0)
+LIBEXEC=$(dirname $MYPATH)
+
+# Handle opts
+src=$1
+dest=$2
+
+
+[ $# -lt 2 ] \
+    && echo "Usage: $0 source dest " \
+    && exit 1
+
+for TYPE in d-lease d-oats; do
+	for FILE in $(/bin/ls ${src}/${TYPE}/*.sig) 
+	do
+		SCHOOL_NAME=`basename ${FILE}`
+		#SCHOOL_NAME=${FILE##*/}
+		SCHOOL_NAME=${SCHOOL_NAME%%.*}
+		#echo $SCHOOL_NAME
+		mkdir -p ${dest}/${SCHOOL_NAME}
+		# copy delegation
+		cp ${FILE} ${dest}/${SCHOOL_NAME}/${TYPE}.sig
+		# this will happen twice -- but we won't
+		# stress about that
+		pushd ${dest}/${SCHOOL_NAME}
+		sha1sum *.sig > manifest.sha1
+		popd
+	done
+done
+

36d3c0c07784d468e63ff945659b741b36e5e2fb
Daniel Drake <dsd@laptop.org>
2011-12-29 03:14:02 -0600

    make-rtcreset: support creating delegated signatures
    Based on code/style found in make-lease.sh

--- build/make-rtcreset.sh
+++ build/make-rtcreset.sh
@@ -11,8 +11,34 @@
 MYPATH=$(readlink -f $0)
 LIBEXEC=$(dirname $MYPATH)
 
-[ $# != 6 ] \
-    && echo "Usage: $0 serial uuid currentrtc nonce newrtc leasekey" >> /dev/stderr \
+# Handle opts
+CHAINFILE=""
+SIGNINGKEY="lease"
+V2=""
+
+while [ $# != 0 ] && [ ${1:0:1} == '-' ]; do
+    case "$1" in
+	--v2)
+	    V2=" --v2 "
+	    ;;
+	--chain)
+	    CHAINFILE=$2
+	    V2=" --v2 "
+	    shift;
+	    ;;
+	--signingkey)
+	    SIGNINGKEY=$2
+	    shift;
+	    ;;
+	*)
+	    echo "Unknown param $1" >> /dev/stderr
+	    exit 1;
+    esac
+    shift
+done
+
+[ $# != 5 ] \
+    && echo "Usage: $0 [--chain delegfile] [--signingkey keyname] serial uuid currentrtc nonce newrtc" >> /dev/stderr \
     && exit 1
 
 SN="$1"
@@ -20,15 +46,27 @@ UUID="$2"
 CURRENTRTC="$3"
 NONCE="$4"
 NEWRTC="$5"
-SIGNINGKEY="$6"
 
 # Ensure nonce is zero-padded with length 10
 NONCE="0000000000$NONCE"
 NONCE="${NONCE: -10}"
 
-echo -n ${SN}:${UUID}:${CURRENTRTC}:${NONCE}:${NEWRTC} >signed-data
-
-$LIBEXEC/sig01 sha256 ${SIGNINGKEY} signed-data >this-signature
-echo -n rtc01: ${SN} ${CURRENTRTC} ${NONCE} ${NEWRTC} "" | cat - this-signature
-# rm -f signed-data this-signature
+payload=${SN}:${UUID}:${CURRENTRTC}:${NONCE}:${NEWRTC}
+echo -n rtc01: ${SN} ${CURRENTRTC} ${NONCE} ${NEWRTC} ""
+if [ "$V2" == "" ]; then
+	echo -n $payload | $LIBEXEC/sig01 sha256 ${SIGNINGKEY}
+else
+	payload="${SN}:00000000T000000Z:${payload}"
+    if [ "$CHAINFILE" == "" ]; then
+		# non-chained v2
+		echo -n $payload | \
+			$LIBEXEC/sig01 --v2 00000000T000000Z sha256 $SIGNINGKEY
+	else
+		# chained v2
+		head -c -1 "$CHAINFILE"
+		echo -n $payload \
+			| $LIBEXEC/sig01 --v2 00000000T000000Z --fullkey sha256 $SIGNINGKEY \
+			| tail -c +7
+	fi
+fi
 
